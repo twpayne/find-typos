c217038ae78b01e01a12b087d57d3d21517d5d45
Tom Payne <twpayne@gmail.com>
2021-10-24 19:32:36 +0200

    feat: Set exit code

--- main.go
+++ main.go
@@ -17,87 +17,96 @@ var (
 	level  = flag.String("level", "warning", "GitHub Action level (notice, warning, or error)")
 )
 
-func printTyposInFile(tf *TypoFinder, path string) error {
+func printTyposInFile(tf *TypoFinder, path string) (int, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	if !strings.HasPrefix(http.DetectContentType(data), "text/") {
-		return nil
+		return 0, nil
 	}
 
 	s := bufio.NewScanner(bytes.NewReader(data))
 	max := 1024 * 1024
 	s.Buffer(make([]byte, max), max)
 	lineNumber := 0
+	n := 0
 	for s.Scan() {
 		lineNumber++
 		if typos := tf.FindTypos(s.Text()); len(typos) > 0 {
+			n += len(typos)
 			for _, typo := range typos {
 				switch *format {
 				case "github-actions":
 					if _, err := fmt.Printf("::%s file=%s,line=%d,col=%d::%s: typo of %s\n", *level, path, lineNumber, typo.Index+1, typo.S, tf.word); err != nil {
-						return err
+						return n, err
 					}
 				default:
 					if _, err := fmt.Printf("%s:%d:%d: %s\n", path, lineNumber, typo.Index+1, typo.S); err != nil {
-						return err
+						return n, err
 					}
 				}
 			}
 		}
 	}
-	return s.Err()
+	return n, s.Err()
 }
 
-func printTyposInStdin(tf *TypoFinder) error {
+func printTyposInStdin(tf *TypoFinder) (int, error) {
 	s := bufio.NewScanner(os.Stdin)
 	max := 1024 * 1024
 	s.Buffer(make([]byte, max), max)
 	lineNumber := 0
+	n := 0
 	for s.Scan() {
 		lineNumber++
 		if typos := tf.FindTypos(s.Text()); len(typos) > 0 {
+			n += len(typos)
 			for _, typo := range typos {
 				switch *format {
 				case "github-actions":
 					if _, err := fmt.Printf("::%s line=%d,col=%d::%s: typo of %s\n", *level, lineNumber, typo.Index+1, typo.S, tf.word); err != nil {
-						return err
+						return n, err
 					}
 				default:
 					if _, err := fmt.Printf("%d:%d: %s\n", lineNumber, typo.Index+1, typo.S); err != nil {
-						return err
+						return n, err
 					}
 				}
 			}
 		}
 	}
-	return s.Err()
+	return n, s.Err()
 }
 
-func run() error {
+func run() (bool, error) {
 	flag.Parse()
 
 	if len(flag.Args()) < 1 {
 		fmt.Printf("usage: %s word [path...]\n", filepath.Base(os.Args[0]))
-		return nil
+		return false, nil
 	}
 
 	tf, err := NewTypoFinder(flag.Arg(0))
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	if flag.NArg() == 1 {
-		return printTyposInStdin(tf)
+		n, err := printTyposInStdin(tf)
+		if err != nil {
+			return false, err
+		}
+		return n == 0, nil
 	}
 
+	total := 0
 	fsys := os.DirFS(".")
 	for _, arg := range flag.Args()[1:] {
 		switch info, err := os.Stat(arg); {
 		case err != nil:
-			return err
+			return false, err
 		case info.IsDir():
 			if err := fs.WalkDir(fsys, arg, func(path string, d fs.DirEntry, err error) error {
 				switch {
@@ -111,26 +120,34 @@ func run() error {
 						return nil
 					}
 				case d.Type() == 0:
-					if err := printTyposInFile(tf, path); err != nil {
+					n, err := printTyposInFile(tf, path)
+					total += n
+					if err != nil {
 						return err
 					}
 				}
 				return nil
 			}); err != nil {
-				return err
+				return false, err
 			}
 		case info.Mode().IsRegular():
-			if err := printTyposInFile(tf, arg); err != nil {
-				return err
+			n, err := printTyposInFile(tf, arg)
+			total += n
+			if err != nil {
+				return false, err
 			}
 		}
 	}
-	return nil
+
+	return total == 0, nil
 }
 
 func main() {
-	if err := run(); err != nil {
+	ok, err := run()
+	if err != nil {
 		fmt.Println(err)
+	}
+	if !ok || err != nil {
 		os.Exit(1)
 	}
 }
